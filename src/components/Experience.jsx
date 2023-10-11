db0e0dcc8a7a55fd2f089aa6611b08f4c302feeb
FloodzHubGit <florentin.dekneudt@epsi.fr>
2023-10-11 23:43:24 +0200

    test numéro 789489485

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -12,6 +12,7 @@ import {
   Controllers,
 } from "@react-three/xr";
 import Interface from "./Interface";
+import { Plane } from "@react-three/drei";
 
 export const Experience = () => {
   return (
@@ -19,16 +20,19 @@ export const Experience = () => {
       <TeleportationPlane leftHand rightHand />
       <Controllers />
       <ambientLight intensity={1} />
-      <group position={[5, -0.1, -5]}>
+      <Plane args={[10, 10, 10, 10]} rotation-x={-Math.PI / 2} position-y={-1}>
+        <meshBasicMaterial wireframe color="red" />
+      </Plane>
+      {/* <group position={[5, -0.1, -5]}>
         <Office />
-      </group>
-      <RayGrab>
+      </group> */}
+      {/* <RayGrab>
         <John />
       </RayGrab>
       <Emma />
-      <Phone />
+      <Phone /> */}
 
-      <Interface />
+      {/* <Interface /> */}
     </>
   );
 };
