c86cee866df76efb1c09041e1e5ad89702ae0f3e
FloodzHubGit <florentin.dekneudt@epsi.fr>
2023-10-12 11:20:02 +0200

    fix text

--- src/components/john/ConversationJohn.jsx
+++ src/components/john/ConversationJohn.jsx
@@ -6,10 +6,7 @@ import { Interactive } from "@react-three/xr";
 import * as THREE from "three";
 import { useEffect } from "react";
 
-const material = new THREE.MeshMatcapMaterial();
-
 export default function ConversationJohn() {
-  const johnTextShowing = store((state) => state.johnTextShowing);
   const [accepted, setAccepted] = useState(false);
 
   const handleAccept = () => {
@@ -22,23 +19,6 @@ export default function ConversationJohn() {
     store.setState({ johnQuestDone: true });
     store.setState({ emmaQuestActive: true });
     store.setState({ currentQuest: "Parler à Emma" });
-
-    const personHelped = store((state) => state.personHelped);
-    const totalPersonToHelp = store((state) => state.totalPersonToHelp);
-    const currentQuest = store((state) => state.currentQuest);
-
-    const [matcapTexture] = useMatcapTexture(
-      "7B5254_E9DCC7_B19986_C8AC91",
-      256
-    );
-
-    useEffect(() => {
-      matcapTexture.colorSpace = THREE.SRGBColorSpace;
-      matcapTexture.needsUpdate = true;
-
-      material.matcap = matcapTexture;
-      material.needsUpdate = true;
-    }, []);
   };
 
   return (
@@ -61,7 +41,7 @@ export default function ConversationJohn() {
           </Interactive>
         </>
       ) : (
-        <Center position={[1, 2, 4.5]} rotation={[0, Math.PI, 0]}>
+        <Center position={[1.5, 2, 4.5]} rotation={[0, Math.PI, 0]}>
           <Text scale={0.08} height={0.2}>
             Salut, je suis John, le supérieur de cet étage. Aide les personnes
             qui ont besoin d'aide.
